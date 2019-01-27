40190521827a1e7f73bf9f740f7004c63be3efbe
Tadej Gregorcic <tadej@motiviti.com>
2019-01-27 21:14:14 +0100

    first little unit test added

--- commands/commands.go
+++ commands/commands.go
@@ -20,6 +20,7 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 
+// package commands includes all the command and subcommand processing
 package commands
 
 import (
